8706fdf11abbf25279ee413755aa14a2897b0afe
Keyel <kovacs.laszlo@ext.telekom.hu>
2024-02-18 16:14:18 +0100

    Add project files.

---
+++ Services/Worker.cs
@@ -0,0 +1,28 @@
+﻿using MassTransit;
+namespace GettingStarted.Services;
+
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using GettingStarted.Contracts;
+
+
+public class Worker(IBus bus) : BackgroundService
+{
+    readonly IBus _bus = bus;
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            await _bus.Publish(new GettingStarted { Value = $"The time is {DateTimeOffset.Now}" }, stoppingToken);
+
+            await Task.Delay(1000, stoppingToken);
+        }
+    }
+}
+
